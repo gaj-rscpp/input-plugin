8ffdb1bcbe5279b0a832f2600a59559ee73376c6
fidoboy <fidoboy@users.noreply.github.com>
2015-08-16 13:16:05 +0200

    Update AGNativeInput.m

--- src/ios/AGNativeInput.m
+++ src/ios/AGNativeInput.m
@@ -522,7 +522,6 @@ int RIGHT_BUTTON_ARG = 3;
 
 - (void) dealloc
 {
-	self.inputView = nil;
 	[[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillShowNotification object:nil];
 	[[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillHideNotification object:nil];
 	[[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillChangeFrameNotification object:nil];
